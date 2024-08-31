4276ae1abd9438435c14bb96db77164370141ed6
acep nurman sidik <acepnurmansidik@gmail.com>
2024-09-01 01:05:34 +0700

    laravel-eloquent orm

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,14 @@
 
     @foreach ($posts as $post)
         <article class="py-5 max-w-screen-md border-b border-x-gray-300">
-            <a href="/posts/{{ $post["slug"] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post["title"] }}</h2>
+            <a href="/posts/{{ $post->slug }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post["author"] }}</a> | 31 Agustus 2024
+                <a href="#">{{ $post->author }}</a> | {{ $post->created_at->diffForHumans() }}
             </div>
-            <p class="my-4 font-light">{{ Str::limit($post["body"], 100) }}</p>
-            <a href="/posts/{{ $post["slug"] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+            <p class="my-4 font-light">{{ Str::limit($post->body, 300) }}</p>
+            <a href="/posts/{{ $post->slug }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,8 +16,8 @@ Route::get('/posts', function () {
     return view('posts',["title"=>"Blog", "posts" => Post::all()]);
 });
 
-Route::get("/posts/{slug}",function($slug){
-    return view('post',["title" => "Single post", "post" => Post::findOne($slug)]);
+Route::get("/posts/{post:slug}",function(Post $post){
+    return view('post',["title" => "Single post", "post" => $post]);
 });
 
 Route::get('/contact', function () {
